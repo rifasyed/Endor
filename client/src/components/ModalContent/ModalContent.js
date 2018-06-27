d37b9000e185f291642a8a6e79606034dbef36d8
jnovoa29 <jordan.novoa15@gmail.com>
2018-06-26 19:54:27 -0700

    login modal css bugs fixed, remaining modals in progress

--- client/src/components/ModalContent/ModalContent.js
+++ client/src/components/ModalContent/ModalContent.js
@@ -9,7 +9,7 @@ const ModalContent = props => {
     return (
     <div className="row">
         <Col s={12}>
-            <Modal id='dialTap' className="tap-target black lighten-3" data-activities="dialBtn"
+            <Modal className= "madmodal black lighten-3" data-activities="dialBtn"
                 trigger={props.iconClicker}>
                 <h5 id="realTime">{props.title}</h5>
                 <span id="monitorPar">The ENDOR[ <Icon id="smallInfinity" className="material-icons" >{props.icon}</Icon> ] {props.description}
